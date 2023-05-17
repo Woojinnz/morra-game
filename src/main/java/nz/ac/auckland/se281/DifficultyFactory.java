15a32c25f1d98bdbb953552f412abe664764405e
Woojin <wjeo779@aucklanduni.ac.nz>
2023-05-18 01:39:38 +1200

    Fixed more to match with codestyle

--- src/main/java/nz/ac/auckland/se281/DifficultyFactory.java
+++ src/main/java/nz/ac/auckland/se281/DifficultyFactory.java
@@ -4,30 +4,31 @@ import java.util.ArrayList;
 import nz.ac.auckland.se281.Main.Difficulty;
 
 public class DifficultyFactory {
-// This is the Factory Design method, where the factory decides the difficulty in realtime.
+  // This is the Factory Design method, where the factory decides the difficulty in realtime.
   public static Intensity createLevel(
       Difficulty intensity, int count, int humanSum, ArrayList<Integer> humanFingers) {
-
+    // This is the factory method, which decides which difficulty to use based on the difficlty
     Intensity result;
     switch (intensity) {
+        // This is the easy difficulty, which is the same as the random strategy
       case EASY:
         {
           result = new EasyDifficulty();
           break;
         }
-
+        // This is the medium difficulty, which is the same as the average + random strategy
       case MEDIUM:
         {
           result = new Medium(count, humanSum);
           break;
         }
-
+        // This is the hard difficulty, which is the same as the top + random strategy
       case HARD:
         {
           result = new HardDifficulty(count, humanSum, humanFingers);
           break;
         }
-
+        // This is the master difficulty, which is the same as the top + average + random strategy
       case MASTER:
         {
           result = new Master(count, humanSum, humanFingers);
@@ -37,7 +38,7 @@ public class DifficultyFactory {
       default:
         return null;
     }
-
+    // This is the code block which runs the strategy
     if (result != null) {
       result.code();
     }
--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -27,9 +27,12 @@ public class Morra {
     name = options[0];
     humanWins = 0;
     aiWins = 0;
+    // Prints welcome to theplayer
     MessageCli.WELCOME_PLAYER.printMessage(name);
     count = 1;
+    // This is important as the difficulty decides the strategy that jarvis will use
     this.difficulty = difficulty;
+    // Set playing to true
     playing = true;
     this.pointsToWin = pointsToWin;
   }
@@ -54,36 +57,38 @@ public class Morra {
   // and such so that jarvis can correctly output its amount of fingers and sum.
   public void play() {
 
+    // Check if playing is true
     if (playing == true) {
       MessageCli.START_ROUND.printMessage(Integer.toString(count));
       MessageCli.ASK_INPUT.printMessage();
 
       while (playing == true) {
-
+        // Get input from the player
         String input = Utils.scanner.nextLine();
-
+        // Check if the input is valid
         if (input.matches("^\\d \\d{1,2}$")) {
 
           String parts[] = input.split(" ", 2);
           int finger = Integer.parseInt(parts[0]);
           int sum = Integer.parseInt(parts[1]);
           humanFingers.add(finger);
-
+          // Check if the input is valid
           if (finger >= 1 && finger <= 5 && sum >= 1 && sum <= 10) {
             MessageCli.PRINT_INFO_HAND.printMessage(
                 name, Integer.toString(finger), Integer.toString(sum));
-
+            // Create a new jarvis object
             Jarvis jarvis = new Jarvis(this);
+            // Get the jarvis output
             String[] ai = jarvis.play();
             String fingerJarvis = ai[0];
             String sumJarvis = ai[1];
-
+            // Print the jarvis output
             MessageCli.PRINT_INFO_HAND.printMessage("Jarvis", fingerJarvis, sumJarvis);
-
+            // Calculate who won
             int victory =
                 calcuateWhoWon(
                     finger, Integer.parseInt(fingerJarvis), sum, Integer.parseInt(sumJarvis));
-
+            // Print the outcome of the round
             switch (victory) {
               case 2:
                 MessageCli.PRINT_OUTCOME_ROUND.printMessage("HUMAN_WINS");
@@ -97,7 +102,7 @@ public class Morra {
               default:
                 MessageCli.PRINT_OUTCOME_ROUND.printMessage("DRAW");
             }
-
+            // Check if the game is over
             if (humanWins == pointsToWin) {
               MessageCli.END_GAME.printMessage(name, Integer.toString(count));
               playing = false;
@@ -105,7 +110,7 @@ public class Morra {
               MessageCli.END_GAME.printMessage("Jarvis", Integer.toString(count));
               playing = false;
             }
-
+            // Increment the count and stratCount
             count++;
             stratCount++;
             humanSum += finger;
@@ -127,10 +132,12 @@ public class Morra {
   // The showStats class, which just prints the current stats of the game and the wins and how much
   // more to win.
   public void showStats() {
+    // So firstly checks if the game is playing or not.
+    // We need it to be in playing mode.
     if (playing == false) {
       MessageCli.GAME_NOT_STARTED.printMessage();
     } else {
-
+      // Calcuations for how many more points till win
       int humanToWin = pointsToWin - humanWins;
       int aiToWin = pointsToWin - aiWins;
       MessageCli.PRINT_PLAYER_WINS.printMessage(
